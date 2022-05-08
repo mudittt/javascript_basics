589752ade93b42f955b8ef51b7569a6c640db88e
mudit tyagi <mudittyagi2002@gmail.com>
2022-05-08 22:38:12 +0530

    adding, removing, arrow functions, searching

---
+++ arrays.js
@@ -0,0 +1,76 @@
+// arrays are also objects, so they have their built in functions and methods
+/*
+numbers.concat
+numbers.copyWithin
+numbers.entries
+numbers.fill
+numbers.filter
+numbers.find
+numbers.findIndex
+*/
+const numbers = [3, 4];
+// end
+numbers.push(5, 6);
+numbers.push(3);
+
+// beginning
+numbers.unshift(1, 2);
+
+// index ; number of items you wanna delete ; items you wanna add...
+numbers.splice(2, 0, "a", "b");
+
+// return the index, if not found returns -1.
+console.log(numbers.indexOf("m"));
+
+// return the index of the latest occurence of that element.
+console.log(numbers.lastIndexOf(3));
+
+// value types ~
+console.log("true if present, false if not present -> ", numbers.includes(0));
+console.log(numbers);
+
+const course = [
+  {
+    id: 1,
+    name: "node.js",
+  },
+  {
+    id: 2,
+    name: "react.js",
+  },
+];
+
+// reference types ~
+const yesORno = course.find(function (courseName) {
+  return courseName.name === "node.js";
+});
+const index = course.findIndex(function (courseName) {
+  return courseName.name === "node.js";
+});
+console.log(yesORno);
+console.log(index);
+
+// arrow function
+const arrow = course.find((courseName) => {
+  return courseName.name === "node.js";
+});
+console.log(arrow);
+
+// removing element
+const marks = [99, 98, 96, 97, 95, 93];
+console.log(" array -> ", marks);
+
+// end
+const lastElement = marks.pop();
+console.log(" removed element (end) -> ", lastElement);
+console.log(" array -> ", marks);
+
+// end
+const firstElement = marks.shift();
+console.log(" removed element (first) -> ", firstElement);
+console.log(" array -> ", marks);
+
+// middle
+const middleElement = marks.splice(2, 1);
+console.log(" removed element (at index) -> ", middleElement);
+console.log(" array -> ", marks);
